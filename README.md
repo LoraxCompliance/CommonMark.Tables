486d38d290b3f9f4292d1e2510cb6a6d530f4627
lorax-alamzedshort <alamzedshort@loraxcompliance.com>
2018-06-27 15:11:54 +0100

    Initial code commit supporting skeletal, non-Markdown-nested tables

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # CommonMark.Tables
 
-A custom formatter to extend CommonMark.NET to render Markdown tables
+A custom formatter to extend [CommonMark.NET](https://github.com/Knagis/CommonMark.NET) to render Markdown tables.
+
+## Example
+
+At the moment, this formatter only supports pipe-delimited columns/data and a skeletal table frame. Therefore,
+
+```
+Column 1 | Column 2
+---|---
+1 | 0
+```
+
+will yield
+
+Column 1 | Column 2
+---|---
+1 | 0
+
+
+## Roadmap
+
+* State-based lookahead to detect table validity
+* Support for a wider variety of table markup
+* Nested Markdown in table headers and cells
+* Robust pipe detection and rejection in cells
+
+## License
+[MIT](https://github.com/LoraxCompliance/CommonMark.Tables/blob/master/LICENSE)
---
+++ TableFormatter.cs
@@ -0,0 +1,81 @@
+﻿using CommonMark.Syntax;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CommonMark.Tables
+{
+    public class TableFormatter : Formatters.HtmlFormatter
+    {
+        // 
+        private readonly Regex tableRowRegex = new Regex(@"([^\|#]*)(\|[^\|#]*)+");
+        private readonly Regex tableDividerRegex = new Regex(@"-+\|-+");
+
+
+        // State detection
+        private BlockTag? LastSeenBlockTag = null;
+        private bool tableStarted = false;
+        private bool tableDividerPresent = false;
+        private bool tableEnded = false;
+
+
+        public TableFormatter(TextWriter target, CommonMarkSettings settings) : base(target, settings) { }
+
+
+        protected override void WriteBlock(Block block, bool isOpening, bool isClosing, out bool ignoreChildNodes)
+        {
+            LastSeenBlockTag = block.Tag;
+            base.WriteBlock(block, isOpening, isClosing, out ignoreChildNodes);
+        }
+
+        protected override void WriteInline(Inline inline, bool isOpening, bool isClosing, out bool ignoreChildNodes)
+        {
+            if (LastSeenBlockTag != BlockTag.Paragraph)
+            {
+                base.WriteInline(inline, isOpening, isClosing, out ignoreChildNodes);
+                return;
+            }
+
+            if (inline.Tag == InlineTag.String)
+            {
+                string str = inline.LiteralContent?.Trim();
+                ignoreChildNodes = false;
+
+                if (!tableStarted && tableRowRegex.IsMatch(str))
+                {
+                    // Table header
+                    tableStarted = true;
+                    tableDividerPresent = false;
+                    tableEnded = false;
+                    Write("<table>");
+
+                    Write($"<tr>{string.Join("\n", str.Split('|').Select(s => $"<th>{s}</th>"))}</tr>");
+                    return;
+                }
+                else if (!tableDividerPresent && tableDividerRegex.IsMatch(str))
+                {
+                    // Table divider
+                    tableDividerPresent = true;
+                    return;
+                }
+                else if (!tableEnded && tableRowRegex.IsMatch(str))
+                {
+                    // Table data rows
+                    Write($"<tr>{string.Join("\n", str.Split('|').Select(s => $"<td>{s}</td>"))}</tr>");
+
+                    if (inline.NextSibling == null)
+                    {
+                        tableStarted = false;
+                        tableEnded = true;
+                        Write("</table>");
+                    }
+
+                    return;
+                }
+            }
+
+            // Fall through: defer to regular formatter
+            base.WriteInline(inline, isOpening, isClosing, out ignoreChildNodes);
+        }
+    }
+}
